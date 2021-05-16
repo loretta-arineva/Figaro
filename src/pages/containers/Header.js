6e1a47ce35cf4dbe2d0e590662b9161cda2fb760
loretta-arineva <wowshifter@gmail.com>
2021-05-16 18:41:08 +0200

    Structured project

---
+++ src/pages/containers/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <Logo />
+            <MainMenu />
+        </header>
+    )
+}
+
+export default Header
