adfa561881e37f463859ed75d80aa1980701f840
loretta-arineva <wowshifter@gmail.com>
2021-05-22 15:09:14 +0200

    770px grid footer style added

--- src/pages/containers/Footer.js
+++ src/pages/containers/Footer.js
@@ -5,39 +5,48 @@ import ListItem from '../components/multifunctional/Navigation/NavListItem';
 const Footer = () => {
     return (
         <footer>
-            <Logo/>
-            <h3>Information</h3>
-            <Navigation>
-                <ListItem>About us</ListItem>
-                <ListItem>Style Guide</ListItem>
-                <ListItem>Book</ListItem>
-            </Navigation>
-
-            <h3>Locations</h3>
-            <Navigation>
-                <ListItem>New York</ListItem>
-                <ListItem>Los Angeles</ListItem>
-                <ListItem>Milan</ListItem>
-            </Navigation>
+            <Logo />
             
-            <h3>Services</h3>
-            <Navigation>
-                <ListItem>Hair Styling</ListItem>
-                <ListItem>Beard Styling</ListItem>
-                <ListItem>Massages</ListItem>
-                <ListItem>Coloring</ListItem>
-            </Navigation>
+            <div>
+                <h3>Information</h3>
+                <Navigation>
+                    <ListItem>About us</ListItem>
+                    <ListItem>Style Guide</ListItem>
+                    <ListItem>Book</ListItem>
+                </Navigation>
+            </div>
 
-            <h3>Follow Us</h3>
-            <Navigation>
-                <ListItem>Facebook</ListItem>
-                <ListItem>Instagram</ListItem>
-                <ListItem>Twitter</ListItem>
-                <ListItem></ListItem>
-            </Navigation>
+            <div>
+                <h3>Locations</h3>
+                <Navigation>
+                    <ListItem>New York</ListItem>
+                    <ListItem>Los Angeles</ListItem>
+                    <ListItem>Milan</ListItem>
+                </Navigation>
+            </div>
 
+            <div>
+                <h3>Services</h3>
+                <Navigation>
+                    <ListItem>Hair Styling</ListItem>
+                    <ListItem>Beard Styling</ListItem>
+                    <ListItem>Massages</ListItem>
+                    <ListItem>Coloring</ListItem>
+                </Navigation>
+            </div>
+
+            <div>
+                <h3>Follow Us</h3>
+                <Navigation>
+                    <ListItem>Facebook</ListItem>
+                    <ListItem>Instagram</ListItem>
+                    <ListItem>Twitter</ListItem>
+                    <ListItem></ListItem>
+                </Navigation>
             <div className="copyright">Created by <a href="https://loretta-arineva.dev/" target="_blank"><span>Loreta Krasteva</span></a></div>
-            
+            </div>
+
+
         </footer>
     )
 }
